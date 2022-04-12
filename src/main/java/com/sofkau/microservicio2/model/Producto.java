0b56a67eb224256ea09a826973367c88effffbf6
EdMass <zairmass@gmail.com>
2022-04-12 15:30:30 -0500

    cambios 4

--- src/main/java/com/sofkau/microservicio2/model/Producto.java
+++ src/main/java/com/sofkau/microservicio2/model/Producto.java
@@ -4,7 +4,7 @@ import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "producto")
+@Document(collection = "productos")
 @NoArgsConstructor
 @RequiredArgsConstructor
 @Data
